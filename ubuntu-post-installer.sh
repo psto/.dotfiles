62b5052f2c9afd5aef855951d1b6832f81bc2485
Piotr Stojanow <piotr.stojanow@gmail.com>
2021-11-06 10:52:03 +0100

    update ubuntu post installer script

--- ubuntu-post-installer.sh
+++ ubuntu-post-installer.sh
@@ -15,10 +15,24 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # install PPA's
-sudo apt-get install -y kitty
+sudo apt-get install -y bat
+sudo apt-get install -y cmatrix
+sudo apt-get install -y exa
+sudo apt-get install -y ffmpeg 
 sudo apt-get install -y fish
+sudo apt-get install -y git
 sudo apt-get install -y htop
+sudo apt-get install -y httpie
+sudo apt-get install -y kitty
 sudo apt-get install -y neofetch
+sudo apt-get install -y mpv
+sudo apt-get install -y preload
+sudo apt-get install -y tlp
+sudo apt-get install -y ranger
+sudo apt-get install -y silversearcher-ag
+sudo apt-get install -y synaptic
+sudo apt-get install -y tmux
+sudo apt-get install -y unrar
 sudo apt-get install -y zathura
 
 # REMOVE some unneeded apps #
@@ -31,22 +45,24 @@ sudo apt-get install smbfs nautilus-open-terminal vim mc openvpn geany smplayer 
 sudo dpkg -i /home/yourname/directory/with/deb/files/*.deb
 
 # Install snap packages
-sudo snap install signal-desktop
-sudo snap install vlc
-sudo snap install keepassxc
-sudo snap install httpie
-sudo snap install insomnia
-sudo snap install gimp
-sudo snap install flameshot
-sudo snap install ffsend
+sudo snap install anki-woodrow
 sudo snap install brave
 sudo snap install chromium
-sudo snap install anki-woodrow
-sudo snap install espanso --classic
 sudo snap install code --classic
+sudo snap install espanso --classic
+sudo snap install flameshot
+sudo snap install ffsend
+sudo snap install gallery-dl
+sudo snap install gimp
+sudo snap install insomnia
+sudo snap install keepassxc
+sudo snap install mailspring
 sudo snap install --edge nvim --classic
+sudo snap install signal-desktop
+sudo snap install vlc
 
 # Flatpak packages
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install flathub com.github.alainm23.planner
 sudo flatpak install flathub io.freetubeapp.FreeTube
+sudo flatpak install flathub md.obsidian.Obsidian
