b2cb2e34083173f904e2eb9c2c59d9e88e36ee43
Piotr Stojanow <piotr.stojanow@gmail.com>
2021-11-13 08:52:58 +0100

    add endeavouros configs

---
+++ endeavouros-i3wm/.screenlayout/i3-dual-monitor.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# generated with arandr
+# location: ~/.screenlayout/
+# if using lightdm and lightdm-gtk-greeter-settings to fix xfce4 login resolution
+# cp script to /etc/lightdm/ and make it executable
+# change [Seat:*] display-setup-script=/etc/lightdm/i3-dual-monitor.sh
+xrandr --output eDP1 --mode 1366x768 --pos 0x672 --rotate normal --output DP1 --off --output HDMI1 --primary --mode 2560x1440 --pos 1366x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
