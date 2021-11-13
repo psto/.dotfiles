9e8a3e4c63344d5da6407c23d4912f8ea23c51f0
Piotr Stojanow <piotr.stojanow@gmail.com>
2021-11-13 18:36:32 +0100

    add endeavouros-post-install script
    add endeavouros-post-install script

---
+++ endeavouros-post-install.sh
@@ -0,0 +1,32 @@
+sudo pacman -Sy bat
+sudo pacman -Sy fish
+sudo pacman -Sy fzf
+sudo pacman -Sy cmatrix
+sudo pacman -Sy exa
+sudo pacman -Sy htop
+sudo pacman -Sy httpie
+sudo pacman -Sy keepassxc
+sudo pacman -Sy ranger
+sudo pacman -Sy ripgrep
+sudo pacman -Sy signal-desktop
+sudo pacman -Sy the_silver_searcher
+sudo pacman -Sy tmux
+sudo pacman -Sy trash-cli
+sudo pacman -Sy tree-sitter
+sudo pacman -Sy qbittorrent
+sudo pacman -Sy xclip
+sudo pacman -Sy zathura
+
+yay -S brave-beta-bin
+yay -S streamlink-twitch-gui
+
+mkdir Programs/ && cd Programs/
+
+git clone https://aur.archlinux.org/dropbox.git
+git clone https://aur.archlinux.org/anki.git
+git clone https://aur.archlinux.org/neovim-nightly-bin.git
+git clone https://aur.archlinux.org/obsidian.git
+git clone https://aur.archlinux.org/rbenv.git
+git clone https://aur.archlinux.org/timeshift.git
+git clone https://aur.archlinux.org/discord_arch_electron.git
+
