04c8a0c2c19d04d33c1df64f330dc30ec78cf80e
Piotr Stojanow <piotr.stojanow@gmail.com>
2021-11-21 12:05:31 +0100

    add neovim lsp completion setup

---
+++ .config/nvim/after/plugin/completion.lua
@@ -0,0 +1,121 @@
+-- -- -- nvim-cmp setup
+-- local cmp = require 'cmp'
+-- local luasnip = require("luasnip")
+--
+-- local has_words_before = function()
+--   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+-- end
+--
+-- cmp.setup {
+--   snippet = {
+--     expand = function(args)
+--       luasnip.lsp_expand(args.body)
+--     end,
+--   },
+--   mapping = {
+--     ['<C-p>'] = cmp.mapping.select_prev_item(),
+--     ['<C-n>'] = cmp.mapping.select_next_item(),
+--     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+--     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--     ['<C-Space>'] = cmp.mapping.complete(),
+--     ['<C-e>'] = cmp.mapping.close(),
+--     ['<CR>'] = cmp.mapping.confirm {
+--       behavior = cmp.ConfirmBehavior.Replace,
+--       select = true,
+--     },
+--     -- ['<Tab>'] = function(fallback)
+--     --   if cmp.visible() then
+--     --     cmp.select_next_item()
+--     --   elseif luasnip.expand_or_jumpable() then
+--     --     luasnip.expand_or_jump()
+--     --   else
+--     --     fallback()
+--     --   end
+--     -- end,
+--     -- ['<S-Tab>'] = function(fallback)
+--     --   if cmp.visible() then
+--     --     cmp.select_prev_item()
+--     --   elseif luasnip.jumpable(-1) then
+--     --     luasnip.jump(-1)
+--     --   else
+--     --     fallback()
+--     --   end
+--     -- end,
+--     ["<Tab>"] = cmp.mapping(function(fallback)
+--       if cmp.visible() then
+--         cmp.select_next_item()
+--       elseif luasnip.expand_or_jumpable() then
+--         luasnip.expand_or_jump()
+--       elseif has_words_before() then
+--         cmp.complete()
+--       else
+--         fallback()
+--       end
+--     end, { "i", "s" }),
+--
+--     ["<S-Tab>"] = cmp.mapping(function(fallback)
+--       if cmp.visible() then
+--         cmp.select_prev_item()
+--       elseif luasnip.jumpable(-1) then
+--         luasnip.jump(-1)
+--       else
+--         fallback()
+--       end
+--     end, { "i", "s" }),
+--   },
+--   sources = {
+--     { name = 'nvim_lsp' },
+--     { name = 'luasnip' },
+--     { name = "path" },
+--     { name = "buffer", keyword_length = 5 },
+--   },
+--   experimental = {
+--     -- I like the new menu better! Nice work hrsh7th
+--     native_menu = false,
+--
+--     -- Let's play with this for a day or two
+--     ghost_text = false,
+--   },
+-- }
+
+-- -- nvim-cmp setup
+local cmp = require 'cmp'
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    { name = "path" },
+    { name = "buffer", keyword_length = 5 },
+  },
+  experimental = {
+    -- I like the new menu better! Nice work hrsh7th
+    native_menu = false,
+
+    -- Let's play with this for a day or two
+    ghost_text = false,
+  },
+}
