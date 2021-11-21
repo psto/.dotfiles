10c688e9b4a44622927e02deae4ac7f858d477ed
psto <piotr.stojanow@gmail.com>
2021-11-21 17:26:27 +0100

    change dir structure to manage dotfiles with stow

--- endeavouros-i3wm/endeavouros-post-install.sh
+++ endeavouros-i3wm/endeavouros-post-install.sh
@@ -1,5 +1,6 @@
 sudo pacman -Sy autorandr
 sudo pacman -Sy bat
+sudo pacman -Sy dash
 sudo pacman -Sy fish
 sudo pacman -Sy fzf
 sudo pacman -Sy cmatrix
@@ -9,9 +10,12 @@ sudo pacman -Sy httpie
 sudo pacman -Sy keepassxc
 sudo pacman -Sy nvm
 sudo pacman -Sy ranger
+sudo pacman -Sy redshift
 sudo pacman -Sy ripgrep
+sudo pacman -Sy stow
 sudo pacman -Sy signal-desktop
 sudo pacman -Sy the_silver_searcher
+sudo pacman -Sy tlp
 sudo pacman -Sy tmux
 sudo pacman -Sy trash-cli
 sudo pacman -Sy tree-sitter
@@ -20,10 +24,14 @@ sudo pacman -Sy xclip
 sudo pacman -Sy zathura
 
 yay -S alacritty
+yay -S btop
 yay -S brave-beta-bin
 yay -S insomnia-bin
+yay -S kitty
 yay -S mailspring
+yay -S nvim-packer-git
 yay -S streamlink-twitch-gui
+yay -S visual-studio-code-bin
 
 mkdir Programs/ && cd Programs/
 
