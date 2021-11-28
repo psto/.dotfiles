a4b411551a92346673aacc4cf1b4522b0a1d7f39
Piotr Stojanow <piotr.stojanow@gmail.com>
2021-11-28 11:29:18 +0100

    add screenlayout for i3

---
+++ endeavouros-i3wm/.screenlayout/monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --mode 1366x768 --pos 0x672 --rotate normal --output DP1 --off --output HDMI1 --primary --mode 2560x1440 --pos 1366x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
