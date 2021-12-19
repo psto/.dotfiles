0fd40dfd20539f29dc473b5142257dae0e51a56a
psto <piotr.stojanow@gmail.com>
2021-12-19 11:24:32 +0100

    replace neoformat with null-ls in neovim & update lsp

--- nvim/.config/nvim/lua/psto/plugins.lua
+++ nvim/.config/nvim/lua/psto/plugins.lua
@@ -111,7 +111,19 @@ return require('packer').startup(function()
 
   -- Formatting
   use 'godlygeek/tabular'
-  use 'sbdchd/neoformat'
+  use "onsails/lspkind-nvim"
+  use({
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      require("null-ls").setup({
+        sources = {
+          require("null-ls").builtins.formatting.prettier,
+          require("null-ls").builtins.diagnostics.eslint,
+        },
+      })
+    end,
+    requires = { "nvim-lua/plenary.nvim" },
+  })
 
   -- Themes
   use 'dracula/vim'
