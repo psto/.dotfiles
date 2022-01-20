de1944d3eb1285f9abd254b68176f1866f54f6b0
psto <piotr.stojanow@gmail.com>
2022-01-20 11:54:36 +0100

    replace nvim snippets completion with luasnip

--- nvim/.config/nvim/after/plugin/completion.lua
+++ nvim/.config/nvim/after/plugin/completion.lua
@@ -1,100 +1,25 @@
--- -- -- nvim-cmp setup
--- local cmp = require 'cmp'
--- local luasnip = require("luasnip")
---
--- local has_words_before = function()
---   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
---   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
--- end
---
--- cmp.setup {
---   snippet = {
---     expand = function(args)
---       luasnip.lsp_expand(args.body)
---     end,
---   },
---   mapping = {
---     ['<C-p>'] = cmp.mapping.select_prev_item(),
---     ['<C-n>'] = cmp.mapping.select_next_item(),
---     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
---     ['<C-f>'] = cmp.mapping.scroll_docs(4),
---     ['<C-Space>'] = cmp.mapping.complete(),
---     ['<C-e>'] = cmp.mapping.close(),
---     ['<CR>'] = cmp.mapping.confirm {
---       behavior = cmp.ConfirmBehavior.Replace,
---       select = true,
---     },
---     -- ['<Tab>'] = function(fallback)
---     --   if cmp.visible() then
---     --     cmp.select_next_item()
---     --   elseif luasnip.expand_or_jumpable() then
---     --     luasnip.expand_or_jump()
---     --   else
---     --     fallback()
---     --   end
---     -- end,
---     -- ['<S-Tab>'] = function(fallback)
---     --   if cmp.visible() then
---     --     cmp.select_prev_item()
---     --   elseif luasnip.jumpable(-1) then
---     --     luasnip.jump(-1)
---     --   else
---     --     fallback()
---     --   end
---     -- end,
---     ["<Tab>"] = cmp.mapping(function(fallback)
---       if cmp.visible() then
---         cmp.select_next_item()
---       elseif luasnip.expand_or_jumpable() then
---         luasnip.expand_or_jump()
---       elseif has_words_before() then
---         cmp.complete()
---       else
---         fallback()
---       end
---     end, { "i", "s" }),
---
---     ["<S-Tab>"] = cmp.mapping(function(fallback)
---       if cmp.visible() then
---         cmp.select_prev_item()
---       elseif luasnip.jumpable(-1) then
---         luasnip.jump(-1)
---       else
---         fallback()
---       end
---     end, { "i", "s" }),
---   },
---   sources = {
---     { name = 'nvim_lsp' },
---     { name = 'luasnip' },
---     { name = "path" },
---     { name = "buffer", keyword_length = 5 },
---   },
---   experimental = {
---     -- I like the new menu better! Nice work hrsh7th
---     native_menu = false,
---
---     -- Let's play with this for a day or two
---     ghost_text = false,
---   },
--- }
+--  nvim-cmp setup
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+  return
+end
+
+require("luasnip/loaders/from_vscode").lazy_load()
 
 -- formatting with lspkind
 local lspkind = require "lspkind"
 lspkind.init()
 
--- -- nvim-cmp setup
-local cmp = require 'cmp'
-
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 cmp.setup {
   snippet = {
     expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
   mapping = {
@@ -108,6 +33,18 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
+    ["<C-l>"] = cmp.mapping(function(fallback)
+      if luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
   },
   formatting = {
     format = lspkind.cmp_format {
@@ -117,20 +54,23 @@ cmp.setup {
         nvim_lsp = "[LSP]",
         nvim_lua = "[api]",
         path = "[path]",
+        luasnip = "[Snippet]",
       },
     },
   },
   sources = {
     { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For vsnip users.
-    { name = "path" },
-    { name = "buffer", keyword_length = 5 },
+    { name = "luasnip" },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    { name = 'treesitter' }, -- TODO: check if works
+    { name = 'path' },
+    { name = 'buffer', keyword_length = 5 },
+  },
+  documentation = {
+    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
   },
   experimental = {
-    -- I like the new menu better! Nice work hrsh7th
     native_menu = false,
-
-    -- Let's play with this for a day or two
     ghost_text = false,
   },
 }
--- nvim/.config/nvim/lua/psto/plugins.lua
+++ nvim/.config/nvim/lua/psto/plugins.lua
@@ -93,17 +93,18 @@ return packer.startup(function(use)
     end
   }
 
-  -- Completion and linting
+  -- Completion, linting and snippets
   use {
     'neovim/nvim-lspconfig',
     'hrsh7th/nvim-cmp',
-    -- 'L3MON4D3/LuaSnip',
-    -- 'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
     'hrsh7th/vim-vsnip',
     'hrsh7th/vim-vsnip-integ',
+    'saadparwaiz1/cmp_luasnip',
+    'L3MON4D3/LuaSnip',
     'rafamadriz/friendly-snippets',
   }
 
