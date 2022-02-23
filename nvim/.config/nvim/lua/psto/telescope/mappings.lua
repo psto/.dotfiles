25c0c0887532eaefd22755f370c1542f2c2298d3
psto <piotr.stojanow@gmail.com>
2022-02-23 20:36:42 +0100

    feat: add new nvim telescope mappings

--- nvim/.config/nvim/lua/psto/telescope/mappings.lua
+++ nvim/.config/nvim/lua/psto/telescope/mappings.lua
@@ -6,7 +6,11 @@ key_map("n", "<leader>fb", [[<CMD>Telescope buffers<CR>]], { noremap = true, sil
 key_map("n", "<leader>fh", [[<CMD>Telescope help_tags<CR>]], { noremap = true, silent = true })
 key_map("n", "<leader>fr", [[<CMD>Telescope oldfiles<CR>]], { noremap = true, silent = true })
 key_map("n", "<leader>fc", [[<CMD>Telescope current_buffer_fuzzy_find<CR>]], { noremap = true, silent = true })
-
+-- search in dotfiles
 key_map("n", "<leader>vrc", [[:lua require('psto.telescope.setup').search_dotfiles()<CR>]], { noremap = true, silent = true })
-key_map("n", "<leader>fw", [[:lua require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }<CR>]], { noremap = true, silent = true })
-
+-- live grep
+key_map("n", "<leader>fl", [[:lua require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }<CR>]], { noremap = true, silent = true })
+-- find word under cursor
+key_map("n", "<leader>fw", "<CMD>Telescope grep_string<CR>", { noremap = true })
+-- find file under cursor
+key_map("n", "<leader>ff", "yiw<CMD>Telescope find_files<CR><C-r>+", { noremap = true })
