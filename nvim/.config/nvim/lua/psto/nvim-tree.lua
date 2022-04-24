170958f80fd66e9217cc3d174d68c35a33a749bc
psto <piotr.stojanow@gmail.com>
2022-04-24 12:27:24 +0200

    fix: migrate nvim-tree setup

--- nvim/.config/nvim/lua/psto/nvim-tree.lua
+++ nvim/.config/nvim/lua/psto/nvim-tree.lua
@@ -42,14 +42,9 @@ nvim_tree.setup {
     "dashboard",
     "alpha",
   },
-  auto_close = false,
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -82,7 +77,6 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "right",
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
@@ -98,15 +92,9 @@ nvim_tree.setup {
     cmd = "trash",
     require_confirm = true,
   },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    }
   },
 }
