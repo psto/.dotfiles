fa6ccd3ae1fe3546033dae1234fc96de149eb26d
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-04-25 21:29:51 +0200

    feat: change nvim theme to tokyo night

--- nvim/.config/nvim/lua/psto/colorscheme.lua
+++ nvim/.config/nvim/lua/psto/colorscheme.lua
@@ -1,18 +1,28 @@
--- local g = vim.g
+local g = vim.g
 -- Colorscheme options
 -- g.tokyonight_transparent = true
 -- g.tokyonight_style = "night"
 -- g.tokyonight_dark_float = false
 -- g.tokyonight_transparent_sidebar = true
+g.tokyonight_colors = { bg_float = "#24283b" }
 -- g.nightflyTransparent = 1
 
 -- Available colorschemes:
--- local colorscheme = "tokyonight"
+local colorscheme = "tokyonight"
 -- local colorscheme = "dracula"
 -- local colorscheme = "duskfox"
 -- local colorscheme = "onedark"
 -- local colorscheme = "nightfly"
-local colorscheme = "kanagawa"
+-- local colorscheme = "kanagawa"
+
+-- TODO: fix must be before pcall, but will throw an error if colorscheme is not found
+-- require'kanagawa'.setup({
+--   overrides  = {
+--     -- set floating window background to transparent
+--     NormalFloat = { bg = "NONE" },
+--     FloatBorder = { bg = "NONE" },
+--   }
+-- })
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
@@ -20,8 +30,3 @@ if not status_ok then
   return
 end
 
-require'kanagawa'.setup({
-  overrides  = {
-    NormalFloat = { bg = "NONE" }, -- set floating window background to transparent
-  }
-})
