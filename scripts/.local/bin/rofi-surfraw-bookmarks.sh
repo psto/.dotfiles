26a09dcbb25657998da49008f5162eb5192c52f0
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-08-18 21:12:08 +0200

    fix: launch bookmarks with surfraw

--- scripts/.local/bin/rofi-surfraw-bookmarks.sh
+++ scripts/.local/bin/rofi-surfraw-bookmarks.sh
@@ -19,4 +19,4 @@
 # set your browser (uncomment if needed, some GUI does not detect browser variable)
 # BROWSER=firefox
 
-surfraw -browser=$BROWSER "$(cat ~/.config/surfraw/bookmarks | sed '/^$/d' | sed '/^#/d' | sed '/^\//d' | sort -n | rofi -config ~/.config/rofi/searchmenu.rasi -dmenu -mesg ">>> Edit to add new bookmarks at ~/.config/surfraw/bookmarks" -i -p "rofi-surfraw-bookmarks: ")"
+surfraw "$(cat ~/.config/surfraw/bookmarks | sed '/^$/d' | sed '/^#/d' | sed '/^\//d' | sort -n | rofi -config ~/.config/rofi/searchmenu.rasi -dmenu -mesg ">>> Edit to add new bookmarks at ~/.config/surfraw/bookmarks" -i -p "rofi-surfraw-bookmarks: ")"
