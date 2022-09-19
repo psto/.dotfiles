201dff4f65e0abd7e410b9be324056d91a0b1998
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-09-19 17:45:46 +0200

    feat: add nvim telescope mapping for file browser

--- nvim/.config/nvim/lua/psto/telescope/mappings.lua
+++ nvim/.config/nvim/lua/psto/telescope/mappings.lua
@@ -1,16 +1,30 @@
 local key_map = vim.api.nvim_set_keymap
 
 key_map("n", "<c-p>", [[<CMD>Telescope find_files<CR>]], { noremap = true, silent = true })
+key_map("n", "<leader>ff", [[<CMD>Telescope find_files<CR>]], { noremap = true, silent = true })
 key_map("n", "<leader>fg", [[<CMD>Telescope live_grep<CR>]], { noremap = true, silent = true })
 key_map("n", "<leader>fb", [[<CMD>Telescope buffers<CR>]], { noremap = true, silent = true })
 key_map("n", "<leader>fh", [[<CMD>Telescope help_tags<CR>]], { noremap = true, silent = true })
-key_map("n", "<leader>fr", [[<CMD>Telescope oldfiles<CR>]], { noremap = true, silent = true })
+key_map("n", "<leader>fo", [[<CMD>Telescope oldfiles<CR>]], { noremap = true, silent = true })
 key_map("n", "<leader>fc", [[<CMD>Telescope current_buffer_fuzzy_find<CR>]], { noremap = true, silent = true })
+key_map("n", "<leader>fr", [[<CMD>Telescope resume<CR>]], { noremap = true, silent = true })
 -- search in dotfiles
-key_map("n", "<leader>vrc", [[:lua require('psto.telescope.setup').search_dotfiles()<CR>]], { noremap = true, silent = true })
+key_map(
+  "n",
+  "<leader>vrc",
+  [[:lua require('psto.telescope.setup').search_dotfiles()<CR>]],
+  { noremap = true, silent = true }
+)
 -- live grep
-key_map("n", "<leader>fl", [[:lua require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }<CR>]], { noremap = true, silent = true })
+key_map(
+  "n",
+  "<leader>fl",
+  [[:lua require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }<CR>]],
+  { noremap = true, silent = true }
+)
 -- find word under cursor
 key_map("n", "<leader>fw", "<CMD>Telescope grep_string<CR>", { noremap = true })
 -- find file under cursor
 key_map("n", "<leader>ff", "yiw<CMD>Telescope find_files<CR><C-r>+", { noremap = true })
+-- telescope file browser
+key_map("n", "<C-n>", ":Telescope file_browser path=%:p:h<CR>", { noremap = true })
