caf2c1288d4ecc68fc5c3860f2a50ef7c6590501
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-09-19 17:43:00 +0200

    feat: style nvim borderless telescope

--- nvim/.config/nvim/lua/psto/colorscheme.lua
+++ nvim/.config/nvim/lua/psto/colorscheme.lua
@@ -9,18 +9,47 @@ local colorscheme = "tokyonight"
 -- local colorscheme = "nightfly"
 -- local colorscheme = "kanagawa"
 
--- TODO: fix must be before pcall, but will throw an error if colorscheme is not found
--- require'kanagawa'.setup({
---   overrides  = {
---     -- set floating window background to transparent
---     NormalFloat = { bg = "NONE" },
---     FloatBorder = { bg = "NONE" },
---   }
--- })
+local tokyonight_status_ok, tokyonight = pcall(require, "tokyonight")
+if tokyonight_status_ok then
+  tokyonight.setup({
+    style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+    transparent = true, -- Enable this to disable setting the background color
+
+    on_highlights = function(hl, c)
+      -- borderless Telescope config
+      hl.TelescopeNormal = {
+        bg = c.bg_dark,
+        fg = c.fg_dark,
+      }
+      hl.TelescopeBorder = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopePromptNormal = {
+        bg = c.bg_dark,
+      }
+      hl.TelescopePromptBorder = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopePromptTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopePreviewTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopeResultsTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+    end,
+  })
+end
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
-
