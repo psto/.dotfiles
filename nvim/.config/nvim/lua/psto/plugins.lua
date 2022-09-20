8ac6cd4ddbe91a744557d42231170527b11db8c8
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-09-20 13:25:14 +0200

    feat: popup nvim packer

--- nvim/.config/nvim/lua/psto/plugins.lua
+++ nvim/.config/nvim/lua/psto/plugins.lua
@@ -29,6 +29,16 @@ if not status_ok then
   return
 end
 
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+    prompt_border = "rounded", -- Border style of prompt popups.
+  },
+}
+
 return packer.startup(function(use)
   -- Packer
   use("wbthomason/packer.nvim")
