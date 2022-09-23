1eb80f52c669186be3720a31fb8b8015d3b23e3e
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-09-23 13:19:49 +0200

    chore: update i3 screenlayout

--- endeavouros-i3wm/.config/screenlayout/i3-dual-monitor.sh
+++ endeavouros-i3wm/.config/screenlayout/i3-dual-monitor.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-# generated with arandr
-# location: ~/.screenlayout/
-# include in your ~/.config/i3/config file: exec --no-startup-id ~/.screenlayout/i3-dual-monitors.sh
-xrandr --output eDP1 --mode 1366x768 --pos 0x672 --rotate normal --output DP1 --off --output HDMI1 --primary --mode 2560x1440 --pos 1366x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
+xrandr --output eDP-1 --mode 1366x768 --pos 0x672 --rotate normal --output HDMI-1 --mode 2560x1440 --pos 1366x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
