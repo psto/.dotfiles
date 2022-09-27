b649e552a4126d2041dc5ee46448f2f2ba82b09b
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-09-27 17:45:28 +0200

    fix: lag in nvim toggleterm

--- nvim/.config/nvim/lua/psto/toggleterm.lua
+++ nvim/.config/nvim/lua/psto/toggleterm.lua
@@ -3,9 +3,9 @@ if not status_ok then
   return
 end
 
-toggleterm.setup({
+toggleterm.setup {
   size = 20,
-  open_mapping = [[<c-\>]],
+  open_mapping = [[<m-0>]],
   hide_numbers = true,
   shade_filetypes = {},
   shade_terminals = true,
@@ -24,12 +24,10 @@ toggleterm.setup({
       background = "Normal",
     },
   },
-})
+}
 
 function _G.set_terminal_keymaps()
   local opts = { noremap = true }
-  vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
   vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
   vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
   vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
