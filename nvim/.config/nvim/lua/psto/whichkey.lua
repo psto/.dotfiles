cb8aa4fb778bf3ab9f8e5bd38aa35725d0578a78
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-09-28 21:43:46 +0200

    feat: add lsp nvim keybindings to whichkey

--- nvim/.config/nvim/lua/psto/whichkey.lua
+++ nvim/.config/nvim/lua/psto/whichkey.lua
@@ -135,6 +135,37 @@ local mappings = {
       "Diff",
     },
   },
+  l = {
+    name = "LSP",
+    a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+    d = { "<cmd>TroubleToggle<cr>", "Diagnostics" },
+    w = {
+      "<cmd>Telescope lsp_workspace_diagnostics<cr>",
+      "Workspace Diagnostics",
+    },
+    f = { "<cmd>lua vim.lsp.buf.format({ async = true })<cr>", "Format" },
+    i = { "<cmd>LspInfo<cr>", "Info" },
+    j = {
+      "<cmd>lua vim.diagnostic.goto_next({buffer=0})<CR>",
+      "Next Diagnostic",
+    },
+    k = {
+      "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>",
+      "Prev Diagnostic",
+    },
+    -- v = { "<cmd>lua require('lsp_lines').toggle()<cr>", "Virtual Text" },
+    l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+    q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
+    r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+    R = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
+    s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+    S = {
+      "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+      "Workspace Symbols",
+    },
+    u = { "<cmd>LuaSnipUnlinkCurrent<cr>", "Unlink Snippet" },
+    v = { "<cmd>lua require('lsp_lines').toggle()<cr>", "Virtual Text" },
+  },
   },
   },
   w = {
