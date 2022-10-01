9e2f5dd0f8d0e83a5069ab2ccfca8a200eab5379
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-10-01 17:06:20 +0200

    feat: update endeavouros post install script

--- endeavouros-post-install.sh
+++ endeavouros-post-install.sh
@@ -15,15 +15,20 @@ sudo pacman -S --noconfirm fd
 sudo pacman -S --noconfirm fish
 sudo pacman -S --noconfirm flameshot
 sudo pacman -S --noconfirm foliate
+sudo pacman -S --noconfirm foot # wayland
 sudo pacman -S --noconfirm fzf
+sudo pacman -S --noconfirm gammastep # wayland
 sudo pacman -S --noconfirm ghostscript
 sudo pacman -S --noconfirm gifski
 sudo pacman -S --noconfirm github-cli
 sudo pacman -S --noconfirm graphicsmagick
+sudo pacman -S --noconfirm grim # wayland
 sudo pacman -S --noconfirm htop
 sudo pacman -S --noconfirm httpie
+sudo pacman -S --noconfirm hyperfine
 sudo pacman -S --noconfirm imagemagick
 sudo pacman -S --noconfirm keepassxc
+sudo pacman -S --noconfirm ksnip
 sudo pacman -S --noconfirm kubo
 sudo pacman -S --noconfirm lazygit
 sudo pacman -S --noconfirm lua-language-server
@@ -45,8 +50,12 @@ sudo pacman -S --noconfirm rofi-calc
 sudo pacman -S --noconfirm scrot
 sudo pacman -S --noconfirm sd
 sudo pacman -S --noconfirm signal-desktop
+sudo pacman -S --noconfirm slurp # wayland
 sudo pacman -S --noconfirm stow
 sudo pacman -S --noconfirm surfraw
+sudo pacman -S --noconfirm swappy # wayland
+sudo pacman -S --noconfirm sway # wayland
+sudo pacman -S --noconfirm swaybg # wayland
 sudo pacman -S --noconfirm thermald
 sudo pacman -S --noconfirm the_silver_searcher
 sudo pacman -S --noconfirm tlp
@@ -59,6 +68,8 @@ sudo pacman -S --noconfirm vlc
 sudo pacman -S --noconfirm xclip
 sudo pacman -S --noconfirm xorg-xbacklight
 sudo pacman -S --noconfirm w3m
+sudo pacman -S --noconfirm waybar # wayland
+sudo pacman -S --noconfirm wofi # wayland
 sudo pacman -S --noconfirm yt-dlp
 sudo pacman -S --noconfirm zathura
 sudo pacman -S --noconfirm zathura-cb
@@ -67,13 +78,21 @@ sudo pacman -S --noconfirm zathura-pdf-mupdf
 sudo pacman -S --noconfirm zoxide
 sudo pacman -S --noconfirm zsh
 
+# for sway
+sudo pacman -S xorg-xwayland xorg-xlsclients qt5-wayland glfw-wayland # wayland
+yay -S swayhide # wayland
+
+# aur
 yay -S alacritty
 yay -S autotiling
 yay -S anki
 yay -S beekeeper-studio-bin
 yay -S btop
 yay -S brave-beta-bin
+yay -S cava
+yay -S clipman # wayland
 yay -S czkawka-cli-bin
+yay -S element-desktop-nightly-bin
 yay -S github-desktop-bin
 yay -S rofi-greenclip
 yay -S devour
@@ -93,11 +112,16 @@ yay -S nvim-packer-git
 yay -S obsidian
 yay -S qrcp-bin
 yay -S rbenv
+yay -S rofi-lbonn-wayland # wayland
 yay -S scitopdf-git
 yay -S sioyek
+yay -S spotify
+yay -S spicetify-cli
 yay -S streamlink-twitch-gui-bin
 yay -S timeshift
 yay -S ulauncher
 yay -S visual-studio-code-bin
+yay -S wdisplays # wayland
+yay -S wlogout # wayland
 yay -S ytfzf
 
