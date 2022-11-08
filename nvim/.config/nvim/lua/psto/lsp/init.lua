54a335cefd81329d99dd4f5ca05b3e920a7f1704
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-11-08 12:59:35 +0100

    fix(nvim): stop sumneko_lua asking about env on everty startup

--- nvim/.config/nvim/lua/psto/lsp/init.lua
+++ nvim/.config/nvim/lua/psto/lsp/init.lua
@@ -431,6 +431,7 @@ nvim_lsp.sumneko_lua.setup({
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
       },
       format = {
         enable = true,
