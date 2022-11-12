37ca468e55f479d72a4d7c92bfceecf48ad513a5
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-11-12 18:49:20 +0100

    feat(nvim): add cmdline & path source completion

--- nvim/.config/nvim/lua/psto/completion.lua
+++ nvim/.config/nvim/lua/psto/completion.lua
@@ -157,3 +157,21 @@ cmp.setup({
     ghost_text = true,
   },
 })
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
