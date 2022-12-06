0e8dffaec4a45003217289c63148948144f8e351
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-12-06 19:58:14 +0100

    feat(nvim): extend ts snippets with js and tsx with jsx

--- nvim/.config/nvim/lua/psto/completion.lua
+++ nvim/.config/nvim/lua/psto/completion.lua
@@ -11,6 +11,8 @@ end
 
 -- specify the path so that friendly-snippets are not duplicated
 require("luasnip.loaders.from_vscode").lazy_load()
+require('luasnip').filetype_extend("typescript", { "javascript" })
+require('luasnip').filetype_extend("typescriptreact", { "javascriptreact" })
 
 local check_backspace = function()
   local col = vim.fn.col(".") - 1
