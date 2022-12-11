b0bdd2b918716204671583efa5c3ca4e83ae18aa
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-12-11 21:17:57 +0100

    feat(nvim): debug with dap, dap-ui & mason-nvim-dap

--- nvim/.config/nvim/lua/psto/whichkey.lua
+++ nvim/.config/nvim/lua/psto/whichkey.lua
@@ -104,6 +104,15 @@ local mappings = {
   q = { '<cmd>lua require("psto/functions").smart_quit()<CR>', "Quit" },
   d = {
     name = "Debug",
+    b = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Breakpoint" },
+    c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
+    i = { "<cmd>lua require'dap'.step_into()<cr>", "Into" },
+    o = { "<cmd>lua require'dap'.step_over()<cr>", "Over" },
+    O = { "<cmd>lua require'dap'.step_out()<cr>", "Out" },
+    r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "Repl" },
+    l = { "<cmd>lua require'dap'.run_last()<cr>", "Last" },
+    u = { "<cmd>lua require'dapui'.toggle()<cr>", "UI" },
+    x = { "<cmd>lua require'dap'.terminate()<cr>", "Exit" },
     s = { "<cmd>lua require('psto.assistance').so_input()<CR>", "  StackOverflow" },
   },
   f = {
