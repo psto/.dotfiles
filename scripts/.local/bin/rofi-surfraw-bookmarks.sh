2ccda1381d481c30612e495ac48c6cbcef604805
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-01-18 20:03:39 +0100

    feat(scripts): change rofi theme to tokyonight

--- scripts/.local/bin/rofi-surfraw-bookmarks.sh
+++ scripts/.local/bin/rofi-surfraw-bookmarks.sh
@@ -19,4 +19,5 @@
 # set your browser (uncomment if needed, some GUI does not detect browser variable)
 # BROWSER=firefox
 
-surfraw "$(cat ~/.config/surfraw/bookmarks | sed '/^$/d' | sed '/^#/d' | sed '/^\//d' | sort -n | rofi -config ~/.config/rofi/searchmenu.rasi -dmenu -mesg ">>> Edit to add new bookmarks at ~/.config/surfraw/bookmarks" -i -p "rofi-surfraw-bookmarks: ")"
+surfraw "$(cat ~/.config/surfraw/bookmarks | sed '/^$/d' | sed '/^#/d' | sed '/^\//d' | sort -n | rofi -config ~/.config/rofi/tokyonight.rasi \
+ -dmenu -mesg ">>> Edit to add new bookmarks at ~/.config/surfraw/bookmarks" -i -p "rofi-surfraw-bookmarks: ")"
