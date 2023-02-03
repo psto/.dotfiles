471c773cd83379486e4b251063b948a7b3e0835c
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-02-03 22:56:28 +0100

    refactor(nvim): move lazy.nvim to config

---
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -0,0 +1,43 @@
+-- Automatically install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " " -- must be set before lazy.nvim
+vim.g.maplocalleader = " "
+
+-- Use a protected call so we don't error out on first use
+local status_ok, lazy = pcall(require, "lazy")
+if not status_ok then return end
+
+return lazy.setup("plugins", {
+  defaults = { lazy = true },
+  install = { colorscheme = { "tokyonight" } },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "rplugin",
+        "tarPlugin",
+        "tohtml",
+        -- "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
