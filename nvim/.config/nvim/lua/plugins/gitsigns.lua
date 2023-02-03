f80cb29bc903f5dcf690ceeb257b464ef7a805a3
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-02-03 23:27:59 +0100

    refactor(nvim): load lazily alpha, colorscheme, gitsigns

---
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,15 @@
+return {
+  "lewis6991/gitsigns.nvim", -- Async signs!
+  event = "BufReadPre",
+  config = function()
+    require("gitsigns").setup({
+      signs = {
+        add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+      },
+    })
+  end
+}
