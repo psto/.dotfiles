282852ee10fd4e2199d695ed7a767f1738210e96
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-02-03 23:27:59 +0100

    refactor(nvim): load lazily alpha, colorscheme, gitsigns

---
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,42 @@
+local M = {
+  "goolord/alpha-nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local dashboard = require "alpha.themes.dashboard"
+    dashboard.section.header.val = {
+      [[              /\ \__          ]],
+      [[ _____    ____\ \ ,_\   ___   ]],
+      [[/\ '__`\ /',__\\ \ \/  / __`\ ]],
+      [[\ \ \L\ \\__, `\\ \ \_/\ \L\ \]],
+      [[ \ \ ,__//\____/ \ \__\ \____/]],
+      [[  \ \ \/ \/___/   \/__/\/___/ ]],
+      [[   \ \_\                      ]],
+      [[    \/_/                      ]],
+    }
+    dashboard.section.buttons.val = {
+      dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("SPC ff", " " .. " Find file", ":Telescope find_files <CR>"),
+      dashboard.button("SPC fo", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("SPC ft", " " .. " Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("SPC vc", " " .. " Config", ":lua require('psto.telescope.setup').search_nvim_dotfiles()<CR>"),
+      dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+    }
+    local function footer()
+      return "stojanow.com"
+    end
+
+    dashboard.section.footer.val = footer()
+
+    dashboard.section.footer.opts.hl = "Type"
+    dashboard.section.header.opts.hl = "Include"
+    dashboard.section.buttons.opts.hl = "Keyword"
+
+    dashboard.opts.opts.noautocmd = true
+    require("alpha").setup(dashboard.opts)
+  end,
+}
+
+return M
---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night", -- three styles: `storm`, a darker variant `night` and `day`
+      transparent = false, -- enable transparency
+
+      on_highlights = function(hl, c)
+        -- borderless Telescope config
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = c.bg_dark,
+        }
+        hl.TelescopePromptBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd('colorscheme tokyonight')
+    end,
+  },
+}
