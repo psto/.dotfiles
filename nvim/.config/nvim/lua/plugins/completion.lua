1be259effa5c24aa2b832dce44d3c60d5f78b6c8
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-02-04 09:40:52 +0100

    refactor(nvim): load lazily cmp, LuaSnip, surround, autopairs, autotag

---
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,136 @@
+return {
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function() require("nvim-surround").setup({}) end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "VeryLazy",
+    config = function() require("nvim-autopairs").setup {} end
+  },
+  {
+    "windwp/nvim-ts-autotag", -- auto close and rename html tag
+    event = "BufReadPre",
+    config = function() require("nvim-ts-autotag").setup() end,
+  },
+  {
+    "L3MON4D3/LuaSnip", -- snippets
+    dependencies = {
+      "psto/friendly-snippets",
+      config = function()
+        -- specify the path so that friendly-snippets are not duplicated
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip").filetype_extend("typescript", { "javascript" })
+        require("luasnip").filetype_extend("typescriptreact", { "javascriptreact" })
+      end,
+    },
+  },
+  -- auto completion
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "saadparwaiz1/cmp_luasnip",
+      "onsails/lspkind-nvim",
+    },
+    opts = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      local check_backspace = function()
+        local col = vim.fn.col(".") - 1
+        return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+      end
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
+
+      return {
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        mapping = cmp.mapping.preset.insert {
+          ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+          ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm { select = false },
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            elseif cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.jumpable(1) then
+              luasnip.jump(1)
+            elseif check_backspace() then
+              fallback()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+          { name = "buffer", keyword_length = 5 },
+        }),
+        formatting = {
+          format = require("lspkind").cmp_format({
+            mode = "symbol",
+          }),
+        },
+        window = {
+          documentation = {
+            border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+          },
+          completion = {
+            border = "rounded",
+            winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+          },
+        },
+        experimental = {
+          ghost_text = {
+            ghost_text = true,
+          },
+        },
+      }
+    end,
+  },
+}
