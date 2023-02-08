779680fb6bd44287f8dabafd58c147c831e0d886
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-02-08 11:45:39 +0100

    fix(nvim): conflict with noice lsp borders & scroll bindings

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -3,7 +3,9 @@ return {
     "karb94/neoscroll.nvim",
     event = "BufReadPre",
     config = function()
-      require("neoscroll").setup({})
+      require("neoscroll").setup({
+        mappings = { '<C-u>', '<C-d>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+      })
     end,
   },
   {
