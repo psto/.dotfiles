96359b1845ecfbc5f94f221b8f2b3ea27f7ab78c
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-02-08 11:58:22 +0100

    feat(nvim): add vim-illuminate

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -8,6 +8,27 @@ return {
     dependencies = "nvim-tree/nvim-web-devicons",
   },
 
+  -- References
+  {
+    "RRethy/vim-illuminate",
+    event = "BufReadPost",
+    opts = { delay = 200 },
+    config = function(_, opts)
+      require("illuminate").configure(opts)
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buffer = vim.api.nvim_get_current_buf()
+          pcall(vim.keymap.del, "n", "]]", { buffer = buffer })
+          pcall(vim.keymap.del, "n", "[[", { buffer = buffer })
+        end,
+      })
+    end,
+    keys = {
+      { "]]", function() require("illuminate").goto_next_reference(false) end, desc = "Next Reference", },
+      { "[[", function() require("illuminate").goto_prev_reference(false) end, desc = "Prev Reference" },
+    },
+  },
+
   -- LSP
   -- {
   --   "glepnir/lspsaga.nvim",
@@ -23,11 +44,4 @@ return {
 
   -- Statusline
   { "SmiteshP/nvim-navic", dependencies = "neovim/nvim-lspconfig", event = "BufReadPre" },
-  -- {
-  --   "folke/noice.nvim",
-  --   dependencies = {
-  --     "MunifTanjim/nui.nvim",
-  --     "rcarriga/nvim-notify",
-  --   }
-  -- },
 }
