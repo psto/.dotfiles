7b80c0be4311d81540cf7d4a5f322e1c9c2c79c6
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-02-08 11:43:54 +0100

    feat(nvim): load lazily noice & notify

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,4 @@
 return {
-
   {
     "lukas-reineke/indent-blankline.nvim",
     event = "BufReadPost",
@@ -41,6 +40,91 @@ return {
     config = function()
       require("nvim-web-devicons").setup({ default = true })
     end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>hn",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Delete all Notifications",
+      },
+    },
+    opts = {
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+    },
+  },
+
+  -- noicer ui
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+        -- signature = { enabled = false },
+        -- hover = { enabled = false }
+      },
+
+      cmdline = {
+        enabled = true, -- enables the Noice cmdline UIview = "cmdline_popup", -- `cmdline` for a classic bottom cmdline or cmdline_popup for popup view
+        view = "cmdline",
+        opts = { buf_options = { filetype = "vim" } }, -- enable syntax highlighting in the cmdline
+        icons = {
+          ["/"] = { icon = " ", hl_group = "DiagnosticWarn" },
+          ["?"] = { icon = " ", hl_group = "DiagnosticWarn" },
+          [":"] = { icon = " ", hl_group = "DiagnosticInfo", firstc = false },
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        lsp_doc_border = true,
+      },
+      routes = {
+        { -- hide written messages
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = true },
+        },
+        { -- show @recording messages
+          view = "notify",
+          filter = { event = "msg_showmode" },
+        },
+        { -- hide empty messages when <S-K> hover
+          view = "notify",
+          filter = { find = "No information available" },
+          opts = { skip = true },
+        },
+      }
+    },
+    keys = {
+      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+      { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+      { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+      { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true,
+        desc = "Scroll forward", mode = { "i", "n", "s" } },
+      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,
+        expr = true, desc = "Scroll backward", mode = { "i", "n", "s" } },
+    },
   },
   { "p00f/nvim-ts-rainbow", event = "BufReadPost" }, -- Highlights
 }
