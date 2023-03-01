fa3604b1f70244369d7bbdf1c5ed8b2fd5324fc8
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-03-01 09:58:31 +0100

    refactor(nvim): concise telescope tokyonight config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,38 +4,22 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      -- style = "night", -- three styles: `storm`, a darker variant `night` and `day`
+      style = "night", -- three styles: `storm`, a darker variant `night` and `day`
       transparent = false, -- enable transparency
 
       on_highlights = function(hl, c)
+        hl.CursorLineNr = { fg = c.orange, bold = true }
+        hl.LineNr = { fg = c.orange, bold = true }
+        hl.LineNrAbove = { fg = c.fg_gutter }
+        hl.LineNrBelow = { fg = c.fg_gutter }
         -- borderless Telescope config
-        hl.TelescopeNormal = {
-          bg = c.bg_dark,
-          fg = c.fg_dark,
-        }
-        hl.TelescopeBorder = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopePromptNormal = {
-          bg = c.bg_dark,
-        }
-        hl.TelescopePromptBorder = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopePromptTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopePreviewTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopeResultsTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
+        hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg_dark }
+        hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
+        hl.TelescopePromptNormal = { bg = c.bg_dark }
+        hl.TelescopePromptBorder = { bg = c.bg_dark, fg = c.bg_dark }
+        hl.TelescopePromptTitle = { bg = c.bg_dark, fg = c.orange }
+        hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
+        hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
       end,
     },
     config = function(_, opts)
