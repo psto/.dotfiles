b25e2dfeb5c745219773f6c1c3e603ece3f3f66f
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-09-15 13:44:17 +0200

    feat(nvim): add Telescope quickfix keymaps

--- nvim/.config/nvim/lua/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -3,22 +3,22 @@ return {
   event = "VeryLazy",
   opts = {
     plugins = {
-      marks = true, -- shows a list of your marks on ' and `
-      registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+      marks = true,       -- shows a list of your marks on ' and `
+      registers = true,   -- shows your registers on " in NORMAL or <C-r> in INSERT mode
       spelling = {
-        enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+        enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
         suggestions = 20, -- how many suggestions should be shown in the list?
       },
       -- the presets plugin, adds help for a bunch of default keybindings in Neovim
       -- No actual key bindings are created
       presets = {
-        operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-        motions = false, -- adds help for motions
+        operators = false,    -- adds help for operators like d, y, ... and registers them for motion / text object completion
+        motions = false,      -- adds help for motions
         text_objects = false, -- help for text objects triggered after entering an operator
-        windows = true, -- default bindings on <c-w>
-        nav = true, -- misc bindings to work with windows
-        z = true, -- bindings for folds, spelling and others prefixed with z
-        g = true, -- bindings for prefixed with g
+        windows = true,       -- default bindings on <c-w>
+        nav = true,           -- misc bindings to work with windows
+        z = true,             -- bindings for folds, spelling and others prefixed with z
+        g = true,             -- bindings for prefixed with g
       },
     },
     -- add operators that will trigger motion and text object completion
@@ -39,24 +39,24 @@ return {
     },
     popup_mappings = {
       scroll_down = "<c-d>", -- binding to scroll down inside the popup
-      scroll_up = "<c-u>", -- binding to scroll up inside the popup
+      scroll_up = "<c-u>",   -- binding to scroll up inside the popup
     },
     window = {
-      border = "rounded", -- none, single, double, shadow
-      position = "bottom", -- bottom, top
-      margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+      border = "rounded",       -- none, single, double, shadow
+      position = "bottom",      -- bottom, top
+      margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
       padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
       winblend = 0,
     },
     layout = {
-      height = { min = 4, max = 25 }, -- min and max height of the columns
-      width = { min = 20, max = 50 }, -- min and max width of the columns
-      spacing = 3, -- spacing between columns
-      align = "center", -- align columns left, center or right
+      height = { min = 4, max = 25 },                                             -- min and max height of the columns
+      width = { min = 20, max = 50 },                                             -- min and max width of the columns
+      spacing = 3,                                                                -- spacing between columns
+      align = "center",                                                           -- align columns left, center or right
     },
-    ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
+    ignore_missing = true,                                                        -- enable this to hide mappings for which you didn't specify a label
     hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-    show_help = false, -- show help message on the command line when the popup is visible
+    show_help = false,                                                            -- show help message on the command line when the popup is visible
     -- triggers = "auto", -- automatically setup triggers
     -- triggers = {"<leader>"} -- or specify a list manually
     triggers_blacklist = {
@@ -70,12 +70,12 @@ return {
   config = function(_, opts)
     local wk = require("which-key")
     local l_opts = {
-      mode = "n", -- NORMAL mode
+      mode = "n",     -- NORMAL mode
       prefix = "<leader>",
-      buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-      silent = true, -- use `silent` when creating keymaps
+      buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+      silent = true,  -- use `silent` when creating keymaps
       noremap = true, -- use `noremap` when creating keymaps
-      nowait = true, -- use `nowait` when creating keymaps
+      nowait = true,  -- use `nowait` when creating keymaps
     }
     local m_opts = {
       mode = "n",
@@ -129,6 +129,8 @@ return {
         M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
         o = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
         p = { "<cmd>Telescope file_browser path=%:p:h<CR>", "File Browser" },
+        q = { "<cmd>Telescope quickfix <CR>", "Quickfix List" },
+        Q = { "<cmd>Telescope quickfixhistory <CR>", "Quickfix History" },
         r = { "<cmd>Telescope resume<cr>", "Last Search" },
         R = { "<cmd>Telescope registers<cr>", "Registers" },
         s = { "<cmd>Telescope spell_suggest theme=get_cursor<CR>", "Spelling" },
