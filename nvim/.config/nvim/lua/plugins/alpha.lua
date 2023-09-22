4f50417792531c9b93a6a4ea4fbc81a075a64891
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-09-22 15:36:22 +0200

    refactor(nvim): change alpha keymaps & add Lazy button

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -19,11 +19,12 @@ local M = {
     }
     dashboard.section.buttons.val = {
       dashboard.button("e", icons.ui.File .. " New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("SPC ff", icons.ui.Files .. " Find file", ":Telescope find_files <CR>"),
-      dashboard.button("SPC fo", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
-      dashboard.button("SPC ft", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
-      dashboard.button("SPC vc", icons.ui.Gear .. " Config",
-        ":lua require('psto.telescope.setup').search_nvim_dotfiles()<CR>"),
+      dashboard.button("f", icons.ui.Files .. " Find file", ":Telescope find_files <CR>"),
+      dashboard.button("o", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("t", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", icons.ui.Gear .. " Config",
+        ':lua require("plugins.telescope").search_nvim_dotfiles()<CR>'),
+      dashboard.button("l", icons.kind.Event .. " Lazy", ":Lazy<CR>"),
       dashboard.button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
     }
 
