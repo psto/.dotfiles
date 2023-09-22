13e2e5f18daac5c3e3068cbfb8e89b6c5419fdce
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-09-22 15:20:25 +0200

    fix(nvim): migrate neotree follow_current_file

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -5,16 +5,16 @@ return {
   lazy = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",     -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
   opts = {
-    close_if_last_window = false,     -- Close Neo-tree if it is the last window left in the tab
+    close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
     popup_border_style = "rounded",
     enable_git_status = true,
     enable_diagnostics = true,
-    sort_case_insensitive = false,     -- used when sorting files and directories in the tree
-    sort_function = nil,               -- use a custom function for sorting files and directories in the tree
+    sort_case_insensitive = false, -- used when sorting files and directories in the tree
+    sort_function = nil,           -- use a custom function for sorting files and directories in the tree
     -- sort_function = function (a,b)
     --       if a.type == b.type then
     --           return a.path > b.path
@@ -28,14 +28,14 @@ return {
       },
       indent = {
         indent_size = 2,
-        padding = 1,         -- extra padding on left hand side
+        padding = 1, -- extra padding on left hand side
         -- indent guides
         with_markers = true,
         indent_marker = "│",
         last_indent_marker = "└",
         highlight = "NeoTreeIndentMarker",
         -- expander config, needed for nesting files
-        with_expanders = nil,         -- if nil and file nesting is enabled, will enable expanders
+        with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
         expander_collapsed = "",
         expander_expanded = "",
         expander_highlight = "NeoTreeExpander",
@@ -84,7 +84,7 @@ return {
       mappings = {
         ["<space>"] = {
           "toggle_node",
-          nowait = false,           -- disable `nowait` if you have existing combos starting with this char that you want to use
+          nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
         },
         ["<2-LeftMouse>"] = "open",
         ["<cr>"] = "open",
@@ -108,23 +108,23 @@ return {
           "add",
           -- some commands may take optional config options, see `:h neo-tree-mappings` for details
           config = {
-            show_path = "none",             -- "none", "relative", "absolute"
+            show_path = "none", -- "none", "relative", "absolute"
           },
         },
-        ["A"] = "add_directory",         -- also accepts the optional config.show_path option like "add".
+        ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add".
         ["d"] = "delete",
         ["r"] = "rename",
         ["y"] = "copy_to_clipboard",
         ["x"] = "cut_to_clipboard",
         ["p"] = "paste_from_clipboard",
-        ["c"] = "copy",         -- takes text input for destination, also accepts the optional config.show_path option like "add":
+        ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
         -- ["c"] = {
         --  "copy",
         --  config = {
         --    show_path = "none" -- "none", "relative", "absolute"
         --  }
         --}
-        ["m"] = "move",         -- takes text input for destination, also accepts the optional config.show_path option like "add".
+        ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
         ["q"] = "close_window",
         ["R"] = "refresh",
         ["?"] = "show_help",
@@ -135,38 +135,40 @@ return {
     nesting_rules = {},
     filesystem = {
       filtered_items = {
-        visible = false,         -- when true, they will just be displayed differently than normal items
+        visible = false, -- when true, they will just be displayed differently than normal items
         hide_dotfiles = true,
         hide_gitignored = true,
-        hide_hidden = true,         -- only works on Windows for hidden files/directories
+        hide_hidden = true, -- only works on Windows for hidden files/directories
         hide_by_name = {
           --"node_modules"
         },
-        hide_by_pattern = {         -- uses glob style patterns
+        hide_by_pattern = { -- uses glob style patterns
           --"*.meta",
           --"*/src/*/tsconfig.json",
         },
-        always_show = {         -- remains visible even if other settings would normally hide it
+        always_show = { -- remains visible even if other settings would normally hide it
           --".gitignored",
         },
-        never_show = {         -- remains hidden even if visible is toggled to true, this overrides always_show
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
           --".DS_Store",
           --"thumbs.db"
         },
-        never_show_by_pattern = {         -- uses glob style patterns
+        never_show_by_pattern = { -- uses glob style patterns
           --".null-ls_*",
         },
       },
-      follow_current_file = true,       -- This will find and focus the file in the active buffer every
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every
+      },
       -- time the current file is changed while the tree is open.
-      group_empty_dirs = false,         -- when true, empty folders will be grouped together
+      group_empty_dirs = false, -- when true, empty folders will be grouped together
       hijack_netrw_behavior = "open_current",
       -- "open_default", -- netrw disabled, opening a directory opens neo-tree
       -- in whatever position is specified in window.position
       -- "open_current",  -- netrw disabled, opening a directory opens within the
       -- window like netrw would, regardless of window.position
       -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
-      use_libuv_file_watcher = false,       -- This will use the OS level file watchers to detect changes
+      use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
       -- instead of relying on nvim autocmd events.
       window = {
         mappings = {
@@ -183,9 +185,11 @@ return {
       },
     },
     buffers = {
-      follow_current_file = true,       -- This will find and focus the file in the active buffer every
+      follow_current_file = {
+        enabled = true -- This will find and focus the file in the active buffer every
+      },
       -- time the current file is changed while the tree is open.
-      group_empty_dirs = true,          -- when true, empty folders will be grouped together
+      group_empty_dirs = true, -- when true, empty folders will be grouped together
       show_unloaded = true,
       window = {
         mappings = {
