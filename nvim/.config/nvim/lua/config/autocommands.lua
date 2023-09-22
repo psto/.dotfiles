8dbeb2cb877a00bb955a30bf8050410a152324f5
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-09-22 15:41:42 +0200

    chore(nvim): update plugins

--- nvim/.config/nvim/lua/config/autocommands.lua
+++ nvim/.config/nvim/lua/config/autocommands.lua
@@ -5,8 +5,8 @@ autocmd({ "FileType" }, {
   pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
   callback = function()
     vim.cmd [[
-      nnoremap <silent> <buffer> q :close<CR> 
-      set nobuflisted 
+      nnoremap <silent> <buffer> q :close<CR>
+      set nobuflisted
     ]]
   end,
 })
