7e693d3698a2d9872e111b0839d099c430079c43
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-12-11 23:35:00 +0100

    refactor(nvim): change keymaps n/p git hunk & treesitter goto function

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -4,6 +4,14 @@ return {
   config = function()
     local icons = require("util.icons")
     require("gitsigns").setup({
+      on_attach = function(buffer)
+        local gs = package.loaded.gitsigns
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+        end
+        map("n", "[g", gs.next_hunk, "Next Hunk")
+        map("n", "]g", gs.prev_hunk, "Prev Hunk")
+      end,
       signs = {
         add = { hl = "GitSignsAdd", text = icons.ui.BoldLineLeft, numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
         change = {
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -109,19 +109,19 @@ return {
               enable = true,
               set_jumps = true, -- whether to set jumps in the jumplist
               goto_next_start = {
-                [']m'] = '@function.outer',
+                [']f'] = '@function.outer',
                 [']c'] = '@class.outer',
               },
               goto_next_end = {
-                [']M'] = '@function.outer',
+                [']F'] = '@function.outer',
                 [']C'] = '@class.outer',
               },
               goto_previous_start = {
-                ['[m'] = '@function.outer',
+                ['[f'] = '@function.outer',
                 ['[c'] = '@class.outer',
               },
               goto_previous_end = {
-                ['[M'] = '@function.outer',
+                ['[F'] = '@function.outer',
                 ['[C'] = '@class.outer',
               },
             },
