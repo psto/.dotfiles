73787ffe0e442dc4c0ad12c5a41532b01bcedc04
Piotr Stojanow <piotr.stojanow@gmail.com>
2024-01-08 12:10:56 +0100

    feat(nvim): display codeium status in lualine

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -1,28 +1,17 @@
 --AI completion
 return {
-  { "Exafunction/codeium.vim",
+  {
+    "Exafunction/codeium.vim",
     event = "BufReadPre",
     config = function()
       local keymap = vim.keymap.set
 
       keymap('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-      keymap('i', '<c-.>', function() return vim.fn['codeium#CycleCompletions']( -1) end, { expr = true })
+      keymap('i', '<c-.>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
       keymap('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
       keymap('i', '<c-;>', function() return vim.fn['codeium#Accept']() end, { expr = true })
     end
   },
-  -- {
-  --   "jcdickinson/codeium.nvim",
-  --   event = "BufReadPre",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "MunifTanjim/nui.nvim",
-  --     "hrsh7th/nvim-cmp",
-  --   },
-  --   config = function()
-  --     require("codeium").setup({})
-  --   end
-  -- },
   -- { "aduros/ai.vim", event = "BufReadPre" },
   -- {
   --   "jackMort/ChatGPT.nvim",
