3bfc365987633068e8f270a7fceba8f651f8a556
Piotr Stojanow <piotr.stojanow@gmail.com>
2024-06-02 10:49:34 +0200

    feat(nvim): toggle DiffView open/close

--- nvim/.config/nvim/lua/util/functions.lua
+++ nvim/.config/nvim/lua/util/functions.lua
@@ -1,5 +1,19 @@
 local M = {}
 
+-- function M.jump_over_closing_pair()
+--   local line = vim.api.nvim_get_current_line()
+--   local col = vim.api.nvim_win_get_cursor(0)[2]
+--   local char_to_right = string.sub(line, col + 1, col + 1)
+--   local chars_to_jump = { '"', ")", "]", "}", "'", ">" }
+
+--   if vim.tbl_contains(chars_to_jump, char_to_right) then
+--     vim.cmd("stopinsert")
+--     vim.api.nvim_feedkeys("la", "n", true)
+--   else
+--     vim.cmd("stopinsert")
+--   end
+-- end
+
 function M.smart_quit()
   local bufnr = vim.api.nvim_get_current_buf()
   local modified = vim.api.nvim_buf_get_option(bufnr, "modified")
@@ -13,6 +27,18 @@ function M.smart_quit()
     end)
   else
     vim.cmd "q!"
+  end
+end
+
+function M.diffview_toggle()
+  local lib = require("diffview.lib")
+  local view = lib.get_current_view()
+  if view then
+    -- Current tabpage is a Diffview; close it
+    vim.cmd.DiffviewClose()
+  else
+    -- No open Diffview exists: open a new one
+    vim.cmd.DiffviewOpen()
   end
 end
 
