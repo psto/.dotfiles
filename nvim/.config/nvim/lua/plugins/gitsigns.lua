46dafb811adbd11b5d951ddf5c907308822e9c3b
Piotr Stojanow <piotr.stojanow@gmail.com>
2024-06-30 08:58:02 +0200

    chore(nvim): remove deprecated highlight groups in gitsigns & add numhl

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -12,33 +12,14 @@ return {
         map("n", "[g", gs.next_hunk, "Next Hunk")
         map("n", "]g", gs.prev_hunk, "Prev Hunk")
       end,
-      signs = {
-        add = { hl = "GitSignsAdd", text = icons.ui.BoldLineLeft, numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = {
-          hl = "GitSignsChange",
-          text = icons.ui.BoldLineLeft,
-          numhl = "GitSignsChangeNr",
-          linehl = "GitSignsChangeLn"
-        },
-        delete = {
-          hl = "GitSignsDelete",
-          text = icons.ui.Triangle,
-          numhl = "GitSignsDeleteNr",
-          linehl = "GitSignsDeleteLn"
-        },
-        topdelete = {
-          hl = "GitSignsDelete",
-          text = icons.ui.Triangle,
-          numhl = "GitSignsDeleteNr",
-          linehl = "GitSignsDeleteLn"
-        },
-        changedelete = {
-          hl = "GitSignsChange",
-          text = icons.ui.BoldLineLeft,
-          numhl = "GitSignsChangeNr",
-          linehl = "GitSignsChangeLn"
-        },
+      signs     = {
+        add = { text = icons.ui.BoldLineLeft },
+        change = { text = icons.ui.BoldLineLeft },
+        delete = { text = icons.ui.Triangle },
+        topdelete = { text = icons.ui.Triangle },
+        changedelete = { text = icons.ui.BoldLineLeft },
       },
+      numhl     = true, -- Toggle with `:Gitsigns toggle_numhl`
     })
   end
 }
