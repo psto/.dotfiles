fe44d8d12d8ec12d4f310aae5cb4ef192dd9af12
Piotr Stojanow <piotr.stojanow@gmail.com>
2024-10-01 08:47:53 +0200

    refactor(nvim): remove default toggleterm config

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -2,27 +2,14 @@ local M = {
   "akinsho/nvim-toggleterm.lua",
   event = "VeryLazy",
   config = function()
-
     require("toggleterm").setup {
       size = 20,
       open_mapping = [[<m-0>]],
-      hide_numbers = true,
-      shade_filetypes = {},
-      shade_terminals = true,
       shading_factor = 2,
-      start_in_insert = true,
-      insert_mappings = true,
-      persist_size = true,
       direction = "float",
-      close_on_exit = true,
-      shell = vim.o.shell,
       float_opts = {
         border = "curved",
         winblend = 0,
-        highlights = {
-          border = "Normal",
-          background = "Normal",
-        },
       },
     }
 
@@ -49,13 +36,6 @@ local M = {
         width = 100000,
         height = 100000,
       },
-      on_open = function(_)
-        vim.cmd "startinsert!"
-        -- vim.cmd "set laststatus=0"
-      end,
-      on_close = function(_)
-        -- vim.cmd "set laststatus=3"
-      end,
       count = 99,
     }
 
