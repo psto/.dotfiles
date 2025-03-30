dcef96a380172b0c7210d4a7de5ad91c68c227a2
Piotr Stojanow <piotr.stojanow@gmail.com>
2025-03-30 12:14:38 +0200

    feat(nvim): update formatting options & add transparency for Neovide
    - set neovide transparency, background color, and global transparency settings
    - add a helper function to calculate hexadecimal alpha values on transparency
    - change fold method to "expr" to leverage treesitter folding
    - disable breakindent, showbreak, and linebreak for performance on large files
    - modify formatoptions for improve comment formatting and behavior
    - re-apply joinspaces, fillchars, and conceallevel settings for consistency

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -10,6 +10,15 @@ opt.pumblend = 17 -- transparent popup
 opt.wildmode = "longest:full"
 opt.wildoptions = "pum"
 
+-- Helper function for transparency formatting
+local alpha = function()
+  return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+end
+-- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+g.neovide_transparency = 0.8
+g.transparency = 0.8
+g.neovide_background_color = "#0f1117" .. alpha()
+
 opt.showmode = false
 opt.showcmd = true
 opt.cmdheight = 1          -- Height of the command bar
@@ -42,11 +51,12 @@ opt.tabstop = 2      -- insert 2 spaces for a tab
 opt.shiftwidth = 2   -- the number of spaces inserted for each indentation
 opt.expandtab = true -- convert tabs to spaces
 
-opt.breakindent = true
-opt.showbreak = string.rep(" ", 3) -- Make it so that long lines wrap smartly
-opt.linebreak = true
+-- DISBALED for now because make large files slow
+-- opt.breakindent = true
+-- opt.showbreak = string.rep(" ", 3) -- Make it so that long lines wrap smartly
+-- opt.linebreak = true
 
-opt.foldmethod = "indent"
+opt.foldmethod = "expr"
 opt.foldlevel = 0
 opt.foldlevelstart = 99 --- Expand all folds by default
 opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
@@ -64,24 +74,19 @@ opt.shada = { "!", "'1000", "<50", "s10", "h" }
 opt.mouse = "n"
 
 opt.formatoptions = opt.formatoptions
-    - "a" -- Auto formatting is BAD.
-    - "t" -- Don't auto format my code. I got linters for that.
-    + "c" -- In general, I like it when comments respect textwidth
-    + "q" -- Allow formatting comments w/ gq
-    - "o" -- O and o, don't continue comments
-    + "r" -- But do continue when pressing enter.
-    + "n" -- Indent past the formatlistpat, not underneath it.
-    + "j" -- Auto-remove comments if possible.
-    - "2" -- I'm not in gradeschool anymore
+    - "a"                     -- Auto formatting is BAD.
+    - "t"                     -- Don't auto format my code. I got linters for that.
+    + "c"                     -- In general, I like it when comments respect textwidth
+    + "q"                     -- Allow formatting comments w/ gq
+    - "o"                     -- O and o, don't continue comments
+    + "r"                     -- But do continue when pressing enter.
+    + "n"                     -- Indent past the formatlistpat, not underneath it.
+    + "j"                     -- Auto-remove comments if possible.
+    - "2"                     -- I'm not in gradeschool anymore
 
--- set joinspaces
-opt.joinspaces = false -- Two spaces and grade school, we're done
-
--- set fillchars=eob:~
-opt.fillchars = { eob = "~" }
-
--- concealing characters for obsidian.nvim
-opt.conceallevel = 1
+opt.joinspaces = false        -- Two spaces and grade school, we're done
+opt.fillchars = { eob = "~" } -- set fillchars=eob:~
+opt.conceallevel = 1          -- concealing characters for obsidian.nvim
 
 -- set grep to ripgrep
 opt.grepprg = "rg --vimgrep --smart-case --hidden"
@@ -95,15 +100,9 @@ g.netrw_liststyle = 3 -- set tree view type
 g.loaded_netrwPlugin = 1
 g.loaded_netrw = 1
 
---
--- COLOR
---
+-- Color
 opt.colorcolumn = "80" -- hint to keep lines short
 opt.termguicolors = true
-
--- async format on save and quit with lsp-format
--- cmd [[cabbrev wq execute "Format sync" <bar> wq]]
--- cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 -- approrpiately highlight codefences (codeblocks)
 g.markdown_fenced_languages = { "css", "html", "javascript", "typescript", "lua", "vim" }
