5da0aea94bce90073439ec46dee53e84bc241e63
Piotr Stojanow <piotr.stojanow@gmail.com>
2025-03-30 11:08:42 +0200

    feat(nvim): replace nvim-autopairs with mini.pairs

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,27 +1,9 @@
 return {
   {
-    "windwp/nvim-autopairs",
+    'echasnovski/mini.pairs',
+    version = false,
     event = "BufReadPre",
-    opts = {
-      check_ts = true,
-      ts_config = {
-        lua = { "string", "source" },
-        javascript = { "string", "template_string" },
-      },
-      disable_filetype = { "TelescopePrompt", "spectre_panel" },
-      fast_wrap = {
-        map = "<M-e>",
-        chars = { "{", "[", "(", '"', "'" },
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-        offset = 0, -- Offset from pattern match
-        end_key = "$",
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma = true,
-        highlight = 'Search',
-        highlight_grey = 'Comment'
-      },
-    },
-    config = function(_, opts) require("nvim-autopairs").setup(opts) end
+    opts = {}
   },
   {
     "kylechui/nvim-surround",
