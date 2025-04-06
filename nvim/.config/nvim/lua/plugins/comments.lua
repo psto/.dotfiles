279ed029f3452d24f6452d7552ad741d2414e7c1
Piotr Stojanow <piotr.stojanow@gmail.com>
2025-04-06 18:11:40 +0200

    feat(nvim): add ts-comments.nvim
    Replace Comment.nvim & nvim-ts-context-commentstring.

--- nvim/.config/nvim/lua/plugins/comments.lua
+++ nvim/.config/nvim/lua/plugins/comments.lua
@@ -1,21 +1,8 @@
 return {
   {
-    "JoosepAlviste/nvim-ts-context-commentstring", -- jsx/tsx comment support
+    "folke/ts-comments.nvim",
+    opts = {},
     event = "BufReadPre",
-    config = function()
-      require('ts_context_commentstring').setup {
-        enable_autocmd = false,
-      }
-    end
-  },
-  {
-    "numToStr/Comment.nvim",
-    event = "BufReadPre",
-    config = function()
-      require('Comment').setup {
-        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-        ignore = '^$',
-      }
-    end,
-  },
+    enabled = vim.fn.has("nvim-0.10.0") == 1,
+  }
 }
