0a0f254ca3464a70805830ca0ae2073fcf3ca828
Piotr Stojanow <piotr.stojanow@gmail.com>
2026-03-31 20:36:19 +0200

    feat(nvim): migrate to blink.pairs and cleanup
    - replace mini.pairs with blink.pairs
    - cleanup of lualine and satellite configs & remove rainbow-delimiters.
    - standardize options indentation and clean up formatoptions comments.

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,72 +1,105 @@
 return {
-  {
-    "nvim-mini/mini.pairs",
-    version = false,
-    event = "BufReadPre",
-    opts = {}
-  },
-  {
-    "kylechui/nvim-surround",
-    event = "InsertEnter",
-    opts = {}
-  },
-  {
-    "windwp/nvim-ts-autotag", -- auto close and rename html tag
-    event = "BufReadPre",
-    opts = {}
-  },
-  {
-    "L3MON4D3/LuaSnip", -- snippets
-    event = "InsertEnter",
-    dependencies = {
-      "psto/friendly-snippets",
-      config = function()
-        -- specify the path so that friendly-snippets are not duplicated
-        require("luasnip.loaders.from_vscode").lazy_load()
-        require("luasnip").filetype_extend("typescript", { "javascript" })
-        require("luasnip").filetype_extend("typescriptreact", { "javascriptreact" })
-      end,
-    },
-  },
-  -- auto completion
-  {
-    'saghen/blink.cmp',
-    lazy = "VeryLazy",
-    dependencies = {
-      "psto/friendly-snippets",
-      -- 'saadparwaiz1/cmp_luasnip',
-      { 'L3MON4D3/LuaSnip', version = 'v2.*' },
-    },
-    version = '1.*',
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      keymap = {
-        preset = 'super-tab', -- 'super-tab' for mappings similar to vscode
-        ['<C-y>'] = { 'select_and_accept' },
-        ["<C-j>"] = { "select_prev", "fallback" },
-        ["<C-k>"] = { "select_next", "fallback" },
-      },
-      appearance = {
-        nerd_font_variant = 'mono',
-      },
-      completion = {
-        -- 'full' will fuzzy match on the text before _and_ after the cursor
-        keyword = { range = 'full' },
-        documentation = { auto_show = true, auto_show_delay_ms = 500 },
-        ghost_text = { enabled = false },
-      },
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
-      },
-      fuzzy = { implementation = "prefer_rust_with_warning" },
-      snippets = {
-        preset = "luasnip",
-      },
-      signature = { enabled = true }, -- Experimental signature help support
-    },
-    -- allows extending the enabled_providers array elsewhere in your config
-    -- without having to redefine it
-    opts_extend = { "sources.default" }
-  },
+	{
+		"saghen/blink.pairs",
+		version = "*",
+		event = "BufReadPost",
+		dependencies = "saghen/blink.download",
+		--- @module 'blink.pairs'
+		--- @type blink.pairs.Config
+		opts = {
+			mappings = {
+				enabled = true,
+				cmdline = true,
+				disabled_filetypes = {},
+				pairs = {},
+			},
+			highlights = {
+				enabled = true,
+				-- requires require('vim._extui').enable({}), otherwise has no effect
+				cmdline = true,
+				groups = {
+					"BlinkPairsOrange",
+					"BlinkPairsPurple",
+					"BlinkPairsBlue",
+				},
+				unmatched_group = "BlinkPairsUnmatched",
+
+				-- highlights matching pairs under the cursor
+				matchparen = {
+					enabled = true,
+					-- known issue where typing won't update matchparen highlight, disabled by default
+					cmdline = false,
+					-- also include pairs not on top of the cursor, but surrounding the cursor
+					include_surrounding = false,
+					group = "BlinkPairsMatchParen",
+					priority = 250,
+				},
+			},
+			debug = false,
+		},
+	},
+	{
+		"kylechui/nvim-surround",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
+		"windwp/nvim-ts-autotag", -- auto close and rename html tag
+		event = "BufReadPre",
+		opts = {},
+	},
+	{
+		"L3MON4D3/LuaSnip", -- snippets
+		event = "InsertEnter",
+		dependencies = {
+			"psto/friendly-snippets",
+			config = function()
+				-- specify the path so that friendly-snippets are not duplicated
+				require("luasnip.loaders.from_vscode").lazy_load()
+				require("luasnip").filetype_extend("typescript", { "javascript" })
+				require("luasnip").filetype_extend("typescriptreact", { "javascriptreact" })
+			end,
+		},
+	},
+	-- auto completion
+	{
+		"saghen/blink.cmp",
+		lazy = "VeryLazy",
+		dependencies = {
+			"psto/friendly-snippets",
+			-- 'saadparwaiz1/cmp_luasnip',
+			{ "L3MON4D3/LuaSnip", version = "v2.*" },
+		},
+		version = "1.*",
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = {
+				preset = "super-tab", -- 'super-tab' for mappings similar to vscode
+				["<C-y>"] = { "select_and_accept" },
+				["<C-j>"] = { "select_prev", "fallback" },
+				["<C-k>"] = { "select_next", "fallback" },
+			},
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+			completion = {
+				-- 'full' will fuzzy match on the text before _and_ after the cursor
+				keyword = { range = "full" },
+				documentation = { auto_show = true, auto_show_delay_ms = 500 },
+				ghost_text = { enabled = false },
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+			snippets = {
+				preset = "luasnip",
+			},
+			signature = { enabled = true }, -- Experimental signature help support
+		},
+		-- allows extending the enabled_providers array elsewhere in your config
+		-- without having to redefine it
+		opts_extend = { "sources.default" },
+	},
 }
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,135 +1,137 @@
 return {
-  {
-    "nvim-mini/mini.indentscope",
-    event = "BufReadPost",
-    opts = {
-      symbol = "│",
-      options = { try_as_border = true },
-    },
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "help",
-          "alpha",
-          "dashboard",
-          "neo-tree",
-          "Trouble",
-          "trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
-        },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-    end,
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "BufReadPost",
-    main = "ibl",
-    opts = {
-      scope = {
-        enabled = false
-      },
-      indent = {
-        char = "│",
-      },
-    },
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    config = function()
-      local icons = require("util.icons")
+	{
+		"nvim-mini/mini.indentscope",
+		event = "BufReadPost",
+		opts = {
+			symbol = "│",
+			options = { try_as_border = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "BufReadPost",
+		main = "ibl",
+		opts = {
+			scope = {
+				enabled = false,
+			},
+			indent = {
+				char = "│",
+			},
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		config = function()
+			local icons = require("util.icons")
 
-      require("lualine").setup({
-        options = {
-          theme = "tokyonight",
-          component_separators = "",
-          section_separators = "",
-          globalstatus = true,
-          disabled_filetypes = { statusline = { "dashboard", "lazy" } },
-          symbols = {
-            modified = icons.git.LineModified .. " ",
-          }
-        },
-        sections = {
-          lualine_a = { 'mode' },
-          lualine_b = { 'branch' },
-          lualine_c = {
-            'filename',
-            {
-              'diff',
-              colored = true,
-              diff_color = {
-                added    = { fg = '#9ece6a' },
-                modified = { fg = '#e0af68' },
-                removed  = { fg = '#f7768e' },
-              },
-              symbols = { added = icons.git.LineAdded .. ' ', modified = icons.git.LineModified .. ' ', removed = icons.git.LineRemoved .. ' ' },
-              source = nil,
-            },
-          },
-          lualine_x = {
-            'diagnostics',
-            {
-              'vim.fn["codeium#GetStatusString"]()', -- codeium status
-              fmt = function(str)
-                return icons.misc.MagicWand .. " " .. str:lower():match("^%s*(.-)%s*$")
-              end
-            },
-            'encoding', 'fileformat', 'filetype'
-          },
-          lualine_y = { 'progress' },
-          lualine_z = { 'location' }
-        },
-        extensions = { "lazy", "mason", "neo-tree", "nvim-dap-ui", "trouble", "toggleterm" },
-      })
-    end
-  },
-  {
-    "brenoprata10/nvim-highlight-colors",
-    event = "BufReadPre",
-    opts = {}
-  },
-  {
-    url = "https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git", -- rainbow parentheses
-    event = "BufReadPost"
-  },
-  {
-    url = "https://github.com/lewis6991/satellite.nvim",
-    event = "BufReadPost",
-    opts = {
-      handlers = {
-        cursor = {
-          enable = true,
-          symbols = { '⎺', '⎻', '⎼', '⎽' } -- Supports any number of symbols
-        },
-        search = { enable = true },
-        diagnostic = {
-          enable = true,
-          signs = { '-', '=', '≡' },
-          min_severity = vim.diagnostic.severity.HINT,
-        },
-        gitsigns = {
-          enable = true,
-          signs = { -- can only be a single character (multibyte is okay)
-            add = "│",
-            change = "│",
-            delete = "-",
-          },
-        },
-        marks = {
-          enable = true,
-          show_builtins = false, -- shows the builtin marks like [ ] < >
-          key = 'm'
-        },
-        quickfix = { signs = { '-', '=', '≡' } }
-      },
-    }
-  },
+			require("lualine").setup({
+				options = {
+					theme = "tokyonight",
+					component_separators = "",
+					section_separators = "",
+					globalstatus = true,
+					disabled_filetypes = { statusline = { "dashboard", "lazy" } },
+					symbols = {
+						modified = icons.git.LineModified .. " ",
+					},
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+					lualine_c = {
+						"filename",
+						{
+							"diff",
+							colored = true,
+							diff_color = {
+								added = { fg = "#9ece6a" },
+								modified = { fg = "#e0af68" },
+								removed = { fg = "#f7768e" },
+							},
+							symbols = {
+								added = icons.git.LineAdded .. " ",
+								modified = icons.git.LineModified .. " ",
+								removed = icons.git.LineRemoved .. " ",
+							},
+							source = nil,
+						},
+					},
+					lualine_x = {
+						"diagnostics",
+						{
+							'vim.fn["codeium#GetStatusString"]()', -- codeium status
+							fmt = function(str)
+								return icons.misc.MagicWand .. " " .. str:lower():match("^%s*(.-)%s*$")
+							end,
+						},
+						"encoding",
+						"fileformat",
+						"filetype",
+					},
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+				extensions = { "lazy", "mason", "neo-tree", "nvim-dap-ui", "trouble", "toggleterm" },
+			})
+		end,
+	},
+	{
+		"brenoprata10/nvim-highlight-colors",
+		event = "BufReadPre",
+		opts = {},
+	},
+	{
+		url = "https://github.com/lewis6991/satellite.nvim",
+		event = "BufReadPost",
+		opts = {
+			handlers = {
+				cursor = {
+					enable = true,
+					symbols = { "⎺", "⎻", "⎼", "⎽" }, -- Supports any number of symbols
+				},
+				search = { enable = true },
+				diagnostic = {
+					enable = true,
+					signs = { "-", "=", "≡" },
+					min_severity = vim.diagnostic.severity.HINT,
+				},
+				gitsigns = {
+					enable = true,
+					signs = { -- can only be a single character (multibyte is okay)
+						add = "│",
+						change = "│",
+						delete = "-",
+					},
+				},
+				marks = {
+					enable = true,
+					show_builtins = false, -- shows the builtin marks like [ ] < >
+					key = "m",
+				},
+				quickfix = { signs = { "-", "=", "≡" } },
+			},
+		},
+	},
 }
