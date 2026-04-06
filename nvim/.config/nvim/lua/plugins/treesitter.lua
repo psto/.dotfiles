73fffaf2de4127033a18608a35e5cdec04cdbe66
Piotr Stojanow <piotr.stojanow@gmail.com>
2026-04-06 22:00:09 +0200

    fix(nvim): fix conceal_line highlight error & remove textobjects
    - fix conceal_line error when pressing <S-k> to show the definition
      under cursor
    - change event from VeryLazy to lazy false to load treesitter earlier
    - remove nvim-treesitter-textobjects plugin
    - migrate _extui to _core.ui2

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		event = "VeryLazy",
+		lazy = false,
 		build = ":TSUpdate",
 		opts = {
 			ensure_installed = {
@@ -68,77 +68,5 @@ return {
 				-- termcolors = {} -- table of colour name strings
 			},
 		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		event = "BufReadPost",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["aa"] = "@assignment.outer",
-							["ia"] = "@assignment.inner",
-							["ap"] = "@parameter.outer",
-							["ip"] = "@parameter.inner",
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-							["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-						},
-						selection_modes = {
-							["@parameter.outer"] = "v", -- charwise
-							["@function.outer"] = "V", -- linewise
-							["@class.outer"] = "<c-v>", -- blockwise
-						},
-						move = {
-							enable = true,
-							set_jumps = true, -- whether to set jumps in the jumplist
-							goto_next_start = {
-								["]f"] = "@function.outer",
-								["]c"] = "@class.outer",
-							},
-							goto_next_end = {
-								["]F"] = "@function.outer",
-								["]C"] = "@class.outer",
-							},
-							goto_previous_start = {
-								["[f"] = "@function.outer",
-								["[c"] = "@class.outer",
-							},
-							goto_previous_end = {
-								["[F"] = "@function.outer",
-								["[C"] = "@class.outer",
-							},
-						},
-					},
-				},
-			})
-
-			local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-
-			-- Repeat movement with ; and ,
-			-- ensure ; goes forward and , goes backward regardless of the last direction
-			vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-			vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-
-			-- vim way: ; goes to the direction you were moving.
-			-- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-			-- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-
-			-- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-			vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
-			vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
-			vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
-			vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
-		end,
 	},
 }
